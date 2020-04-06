cbdfc8a46ef13e8f2c054582a17fe65b2a36a201
OrlaBr <orlabreslin@yahoo.com>
2020-04-06 14:21:47 +0000

    Styling Navbar and toggle button

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(urls_accounts)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
