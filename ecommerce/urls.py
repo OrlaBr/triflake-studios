8772e4a9d1889c810b170cfe0bfd1020e8928d07
OrlaBr <orlabreslin@yahoo.com>
2020-04-08 10:31:51 +0000

    Create Products.html, add media root to Settings, connect through urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from accounts import urls as urls_accounts
 from django.conf import settings
 from django.conf.urls.static import static
+from products import urls as urls_products
+from products.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^products/', include(urls_products)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+]
