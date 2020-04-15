c77407fa243a5e35d555202496621af1eae1ad73
OrlaBr <orlabreslin@yahoo.com>
2020-04-15 12:31:28 +0000

    Fix databse error in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -95,8 +95,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # }
 # }
 
-DATABASE = {
-    'default': dj_database_url.parse(os.environ.get('DATABSE_URL'))
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
