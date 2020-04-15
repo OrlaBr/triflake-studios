52e02ff5f48b4dae1ce9cdcc711296d7448cd457
OrlaBr <orlabreslin@yahoo.com>
2020-04-15 10:20:48 +0000

    Prepare for Heroku deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
 from os import path
 if path.exists("env.py"):
     import env
@@ -86,11 +88,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# }
+# }
+
+DATABASE = {
+    'default': dj_database_url.parse(os.environ.get('DATABSE_URL'))
 }
 
 
