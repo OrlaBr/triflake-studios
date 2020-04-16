b35e6cecdded7a00b8f4cb851a1884ca071185c5
OrlaBr <orlabreslin@yahoo.com>
2020-04-16 09:13:23 +0000

    Fix image path error in settings file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,7 +149,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
